25277bb4e5c82dc27b6f2e7adf4cd20c407f679d
MaDHouSe79 <madhouse1979@gmail.com>
2022-11-02 11:09:01 +0100

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,12 +5,13 @@ local Translations = {
         ['zone_saved'] = "The zone is saved in (configs/zones.lua) file..",
     },
     data = {
-        ['created_zone'] = "Gemaakt door: %{name} - Datum: %{date}",
-        ['created_by'] = "Zone: %{zone} in Straat: %{street}",
+        ['created_by'] = "Gemaakt door: %{name} - Datum: %{date}",
+        ['created_zone'] = "Zone: %{zone} in Straat: %{street}",
         ['create_zone'] = "Maak zone [naam]",
         ['type_zone_name'] = "Type a zone name.",
     },
 }
+
 Lang = Locale:new({
     phrases = Translations,
     warnOnMissing = true
