656e3ac6f05065c1e9bbebce8d15efc9a1a326fd
MaDHouSe79 <madhouse1979@gmail.com>
2022-11-02 11:25:37 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -17,9 +17,9 @@ local function GetStreetName(entity)
 end
 
 local function RotationToDirection(rotation)
-	local adjustedRotation = {x = (math.pi / 180) * rotation.x, y = (math.pi / 180) * rotation.y, z = (math.pi / 180) * rotation.z}
-	local direction = {x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), z = math.sin(adjustedRotation.x)}
-	return direction
+    local adjustedRotation = {x = (math.pi / 180) * rotation.x, y = (math.pi / 180) * rotation.y, z = (math.pi / 180) * rotation.z}
+    local direction = {x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), z = math.sin(adjustedRotation.x)}
+    return direction
 end
 
 local function RayCastGamePlayCamera(distance)
@@ -27,10 +27,10 @@ local function RayCastGamePlayCamera(distance)
     if not IsGameplayCamRendering() then currentRenderingCam = GetRenderingCam() end
     local cameraRotation = not currentRenderingCam and GetGameplayCamRot() or GetCamRot(currentRenderingCam, 2)
     local cameraCoord = not currentRenderingCam and GetGameplayCamCoord() or GetCamCoord(currentRenderingCam)
-	local direction = RotationToDirection(cameraRotation)
-	local destination =	{x = cameraCoord.x + direction.x * distance, y = cameraCoord.y + direction.y * distance, z = cameraCoord.z + direction.z * distance}
-	local _, b, c, _, e = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, PlayerPedId(), 0))
-	return b, c, e
+    local direction = RotationToDirection(cameraRotation)
+    local destination =	{x = cameraCoord.x + direction.x * distance, y = cameraCoord.y + direction.y * distance, z = cameraCoord.z + direction.z * distance}
+    local _, b, c, _, e = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, PlayerPedId(), 0))
+    return b, c, e
 end
 
 local function CreatePolyZone(coords)
