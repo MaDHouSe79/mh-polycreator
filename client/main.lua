2b4cc704cf00d2e19a08d090a2e0eab02d5c78cc
MaDHouSe79 <madhouse1979@gmail.com>
2022-11-02 11:08:25 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -3,7 +3,6 @@
 --[[ ===================================================== ]]--
 
 local QBCore = exports['qb-core']:GetCoreObject()
-local MHCore = exports['mh-core']:GetMHCore()
 local PlayerData = {}
 local ViewEnabled = false
 local FreeAim = false
@@ -13,14 +12,36 @@ local height = 2.0
 local tmpPoly = nil
 
 
+local function GetStreetName(entity)
+    return GetStreetNameFromHashKey(GetStreetNameAtCoord(GetEntityCoords(entity).x, GetEntityCoords(entity).y, GetEntityCoords(entity).z))
+end
+
+local function RotationToDirection(rotation)
+	local adjustedRotation = {x = (math.pi / 180) * rotation.x, y = (math.pi / 180) * rotation.y, z = (math.pi / 180) * rotation.z}
+	local direction = {x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)), z = math.sin(adjustedRotation.x)}
+	return direction
+end
+
+local function RayCastGamePlayCamera(distance)
+    local currentRenderingCam = false
+    if not IsGameplayCamRendering() then currentRenderingCam = GetRenderingCam() end
+    local cameraRotation = not currentRenderingCam and GetGameplayCamRot() or GetCamRot(currentRenderingCam, 2)
+    local cameraCoord = not currentRenderingCam and GetGameplayCamCoord() or GetCamCoord(currentRenderingCam)
+	local direction = RotationToDirection(cameraRotation)
+	local destination =	{x = cameraCoord.x + direction.x * distance, y = cameraCoord.y + direction.y * distance, z = cameraCoord.z + direction.z * distance}
+	local _, b, c, _, e = GetShapeTestResult(StartShapeTestRay(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, -1, PlayerPedId(), 0))
+	return b, c, e
+end
+
 local function CreatePolyZone(coords)
     local zone = PolyZone:Create(vectors, {name = "zone", minZ = coords.z - 1, maxZ = coords.z + height, debugPoly = true})
     zones[#zones + 1] = zone
     vectors = {}
     if tmpPoly ~= nil then tmpPoly:destroy() end
-    local street = MHCore.Functions.GetStreetName(PlayerPedId())
+    local street = GetStreetName(PlayerPedId())
     TriggerServerEvent('mh-polycreator:server:writezonefile', zones, street)
-    --zone:destroy()
+    zones = {}
+    zone:destroy()
 end
 
 local function RemovePolyPoint(coords)
@@ -39,6 +60,10 @@ local function RemovePolyPoint(coords)
     end
 end
 
+local function DrawALine(position, coords, color)
+    DrawLine(position.x, position.y, position.z, coords.x, coords.y, coords.z, color.r, color.g, color.b, color.a)
+end
+
 function RunViewThread()
     ViewEnabled = true
     Citizen.CreateThread(function()
@@ -47,7 +72,7 @@ function RunViewThread()
             if FreeAim then
                 local color = {r = 255, g = 255, b = 255, a = 200}
                 local position = GetEntityCoords(PlayerPedId())
-                local hit, coords, entity = MHCore.Functions.RayCastGamePlayCamera(100.0)
+                local hit, coords, entity = RayCastGamePlayCamera(100.0)
                 if hit then    
                     if IsControlJustReleased(0, 38) then -- E To Create point
                         vectors[#vectors + 1] = vector2(coords.x, coords.y)
@@ -60,7 +85,7 @@ function RunViewThread()
                         CreatePolyZone(coords)
                     end
                 end
-                MHCore.Functions.DrawLine(position.x, position.y, position.z, coords.x, coords.y, coords.z, color.r, color.g, color.b, color.a)
+                DrawALine(position, coords, color)
             end
         end
     end)
