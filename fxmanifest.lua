f5963e627fb462b9b97dbabdbdfbd27ec49ea9cc
MaDHouSe <madhouse1979@gmail.com>
2024-04-21 23:45:12 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,19 @@
 --[[ ===================================================== ]]--
---[[         QBCore Player Stores System by MaDHouSe       ]]--
+--[[           MH Poly Creator Script by MaDHouSe          ]]--
 --[[ ===================================================== ]]--
 
 fx_version 'cerulean'
 game 'gta5'
 
 author 'MaDHouSe'
-description 'MH Player Stores System'
+description 'MH Poly Creator System'
 version '1.0'
 
 shared_scripts {
-    '@qb-core/shared/locale.lua',
-    'locales/nl.lua', -- change to your language
+    '@ox_lib/init.lua',
+    'core/shared/mconfig.lua',
+    'core/shared/locale.lua',
+    'core/shared/locales/*.lua',
     'config.lua',
     'configs/*.lua'
 }
@@ -22,19 +24,18 @@ client_scripts {
     '@PolyZone/EntityZone.lua',
     '@PolyZone/CircleZone.lua',
     '@PolyZone/ComboZone.lua',
+    'core/client/cl_core.lua',
 	"client/main.lua",
 }
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
+    'core/server/sv_core.lua',
 	"server/main.lua",
 	'server/update.lua',
 }
 
-dependencies {
-    'oxmysql',
-    'qb-core',
-}
+dependencies {'PolyZone', 'ox_lib'}
 
 
 lua54 'yes'
